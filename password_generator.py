791c1676a97756c056b8611a77cc5f81a7730846
Luciano Aarón <lucaaron00@gmail.com>
2024-03-21 22:18:47 -0600

    Version two of password generator

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,7 @@
 import random
+import os
 
+usage: str = input('Password of...')
 size: int = int(input('Put the size of the password: '))
 password: str = ''
 
@@ -8,11 +10,25 @@ def generate_character()-> str:
 	character = chr(random_number)
 	yield character
 
-
+#Generate the password
 for longitud in range(size):
 	for c in generate_character():
 		password+=c
 
+#Saving the passwords
+FILE_NAME: str = '.Password_Manager.txt'
 
-print('Password: ')
-print(password)
+if os.path.exists(FILE_NAME):
+    with open(FILE_NAME, 'a') as file:
+        file.write('\n--------------------------------------------------\n')
+        file.write(f'Password for {usage}:\n')
+        file.write(password+'\n')
+else:
+    with open(FILE_NAME, 'w') as file:
+        file.write('Password Manager\n')
+        file.write('--------------------------------------------------\n')
+        file.write(f'Password for {usage}:\n')
+        file.write(password+'\n')
+
+print('Password: ', password)
+print(f'Save in {FILE_NAME}')
